1eaa30728ad364eb18a133d02880a417378bb1fb
Diego Confessor <diegondonascimento@gmail.com>
2024-07-22 14:52:33 -0300

    melhorias  na interface

--- js/autopark.js
+++ js/autopark.js
@@ -242,3 +242,19 @@ document.getElementById('searchBtn').addEventListener('click', () => {
     resultadoPesquisaContainer.textContent = 'Veículo não encontrado.';
   }
 });
+
+
+function openNav() {
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelector('.openbtn').addEventListener('click', openNav);
+  document.querySelector('.closebtn').addEventListener('click', closeNav);
+});
