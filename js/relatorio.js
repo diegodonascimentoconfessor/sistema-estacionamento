b21570a37f6217da7333b49c1ac79e530b1b007a
unknown <diegondonascimento@gmail.com>
2024-07-21 21:06:55 -0300

    implementando melhorias

--- js/relatorio.js
+++ js/relatorio.js
@@ -1,15 +1,14 @@
-// Função para carregar dados do localStorage e exibir na tabela
+
 function carregarPagamentos() {
-    // Buscar dados do localStorage
+    
     const pagamentos = JSON.parse(localStorage.getItem('pagamentos')) || [];
   
-    // Selecionar o corpo da tabela
     const tabelaPagamentos = document.getElementById('tabelaPagamentos');
   
-    // Limpar tabela antes de inserir novos dados
+ 
     tabelaPagamentos.innerHTML = '';
   
-    // Iterar sobre os pagamentos e criar linhas na tabela
+   
     pagamentos.forEach(pagamento => {
       const linha = document.createElement('tr');
   
@@ -29,6 +28,6 @@ function carregarPagamentos() {
     });
   }
   
-  // Carregar pagamentos ao carregar a página
+
   window.onload = carregarPagamentos;
   
