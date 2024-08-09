f29511333b6a54465bc667ed335925b5bc8f623b
Diego Confessor <diegondonascimento@gmail.com>
2024-08-09 14:25:14 -0300

    melhorias

--- js/relatorio.js
+++ js/relatorio.js
@@ -1,19 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-  carregarVeiculos();
-});
+document.addEventListener('DOMContentLoaded', carregarRelatorio);
 
-function carregarVeiculos() {
-  const vehicles = JSON.parse(localStorage.getItem('vehicles')) || [];
-  const vehiclesTableBody = document.getElementById('vehiclesTable').getElementsByTagName('tbody')[0];
+function carregarRelatorio() {
+  const pagamentos = JSON.parse(localStorage.getItem('pagamentos')) || [];
+  const paymentsTableBody = document.getElementById('paymentsTableBody');
+  let totalValue = 0;
 
-  vehicles.forEach(vehicle => {
-    const row = vehiclesTableBody.insertRow();
+  pagamentos.forEach(pagamento => {
+    const row = paymentsTableBody.insertRow();
     
-    row.insertCell(0).textContent = vehicle.placa;
-    row.insertCell(1).textContent = vehicle.marcaModelo;
-    row.insertCell(2).textContent = new Date(vehicle.entrada).toLocaleString();
-    row.insertCell(3).textContent = vehicle.tolerancia;
-    row.insertCell(4).textContent = vehicle.tarifa;
-    row.insertCell(5).textContent = vehicle.valorRecebido ? `R$ ${vehicle.valorRecebido.toFixed(2)}` : 'N/A'; // Exibe o valor recebido, se disponível
+    row.insertCell(0).textContent = pagamento.id;
+    row.insertCell(1).textContent = pagamento.placa;
+    row.insertCell(2).textContent = `R$ ${pagamento.valorRecebido.toFixed(2)}`;
+    row.insertCell(3).textContent = new Date(pagamento.data).toLocaleString();
+
+    totalValue += pagamento.valorRecebido;
   });
+
+  document.getElementById('totalValue').textContent = `R$ ${totalValue.toFixed(2)}`;
 }
