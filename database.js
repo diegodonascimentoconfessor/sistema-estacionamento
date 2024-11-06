19ec4b8788e17829625ae665b726b489bd8aa62d
diegodonascimentoconfessor <153656081+diegodonascimentoconfessor@users.noreply.github.com>
2024-11-06 16:24:31 -0300

    Melhorias

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ const port = 3000;
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'TechConnect',
+    database: 'Autopark',  // Alterado para o nome do banco de dados do estacionamento
     password: 'BemVindo!',
     port: 5432,
 });
@@ -17,170 +17,98 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Rota principal (tela inicial)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'app', 'index.html'));
 });
 
-app.post('/cliente', async (req, res) => {
-    const { nome, cpf, datanascimento, celular, endereco } = req.body;
-    const queryText = 'INSERT INTO cliente (nome, cpf, datanascimento, celular, endereco) VALUES ($1, $2, $3, $4, $5) RETURNING *';
-    const values = [nome, cpf, datanascimento, celular, endereco];
+// Rota para adicionar um novo veículo
+app.post('/veiculo', async (req, res) => {
+    const { marcaModelo, placa, cor } = req.body;
+    const entrada = new Date().toISOString(); // Entrada do veículo (data e hora)
+    const queryText = 'INSERT INTO veiculo (marca_modelo, placa, cor, entrada) VALUES ($1, $2, $3, $4) RETURNING *';
+    const values = [marcaModelo, placa, cor, entrada];
 
     try {
         const result = await pool.query(queryText, values);
         res.status(201).json(result.rows[0]);
     } catch (err) {
-        console.error('Erro ao cadastrar cliente:', err);
+        console.error('Erro ao cadastrar veículo:', err);
         res.status(500).send('Erro interno ao processar a solicitação');
     }
 });
 
-app.get('/cliente', async (req, res) => {
+// Rota para listar todos os veículos cadastrados
+app.get('/veiculo', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM cliente');
+        const result = await pool.query('SELECT * FROM veiculo');
         res.json(result.rows);
     } catch (err) {
-        res.status(400).send('Erro ao consultar clientes');
+        res.status(400).send('Erro ao consultar veículos');
     }
 });
 
-app.put('/cliente/:id', async (req, res) => {
+// Rota para editar um veículo
+app.put('/veiculo/:id', async (req, res) => {
     const { id } = req.params;
-    const { nome, cpf, datanascimento, celular, endereco } = req.body;
-    const queryText = 'UPDATE cliente SET nome = $1, cpf = $2, datanascimento = $3, celular = $4, endereco = $5 WHERE id = $6 RETURNING *';
-    const values = [nome, cpf, datanascimento, celular, endereco, id];
+    const { marcaModelo, placa, cor } = req.body;
+    const queryText = 'UPDATE veiculo SET marca_modelo = $1, placa = $2, cor = $3 WHERE id = $4 RETURNING *';
+    const values = [marcaModelo, placa, cor, id];
 
     try {
         const result = await pool.query(queryText, values);
         res.json(result.rows[0]);
     } catch (err) {
-        console.error('Erro ao editar cliente:', err);
+        console.error('Erro ao editar veículo:', err);
         res.status(500).send('Erro interno ao processar a solicitação');
     }
 });
 
-app.delete('/cliente/:id', async (req, res) => {
+// Rota para excluir um veículo
+app.delete('/veiculo/:id', async (req, res) => {
     const { id } = req.params;
-    const queryText = 'DELETE FROM cliente WHERE id = $1 RETURNING *';
+    const queryText = 'DELETE FROM veiculo WHERE id = $1 RETURNING *';
     const values = [id];
 
     try {
         const result = await pool.query(queryText, values);
         res.json(result.rows[0]);
     } catch (err) {
-        console.error('Erro ao deletar cliente:', err);
+        console.error('Erro ao deletar veículo:', err);
         res.status(500).send('Erro interno ao processar a solicitação');
     }
 });
 
-app.post('/produto', async (req, res) => {
-    const { nome, fabricante, modelo, ano, descricao, preco } = req.body;
-    const queryText = 'INSERT INTO produto (nome, fabricante, modelo, ano, descricao, preco) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
-    const values = [nome, fabricante, modelo, ano, descricao, preco];
+// Rota para registrar o pagamento de um veículo
+app.post('/pagamento', async (req, res) => {
+    const { veiculoId, valorPago, dataPagamento } = req.body;
+    const queryText = 'INSERT INTO pagamento (veiculo_id, valor_pago, data_pagamento) VALUES ($1, $2, $3) RETURNING *';
+    const values = [veiculoId, valorPago, dataPagamento];
 
     try {
         const result = await pool.query(queryText, values);
         res.status(201).json(result.rows[0]);
     } catch (err) {
-        console.error('Erro ao cadastrar produto:', err);
+        console.error('Erro ao registrar pagamento:', err);
         res.status(500).send('Erro interno ao processar a solicitação');
     }
 });
 
-app.get('/produto', async (req, res) => {
+// Rota para listar todos os pagamentos
+app.get('/pagamento', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM produto');
+        const result = await pool.query('SELECT * FROM pagamento');
         res.json(result.rows);
     } catch (err) {
-        res.status(400).send('Erro ao consultar produtos');
+        res.status(400).send('Erro ao consultar pagamentos');
     }
 });
 
-app.put('/produto/:id', async (req, res) => {
-    const { id } = req.params;
-    const { nome, fabricante, modelo, ano, descricao, preco } = req.body;
-    const queryText = 'UPDATE produto SET nome = $1, fabricante = $2, modelo = $3, ano = $4, descricao = $5, preco = $6 WHERE id = $7 RETURNING *';
-    const values = [nome, fabricante, modelo, ano, descricao, preco, id];
-
-    try {
-        const result = await pool.query(queryText, values);
-        res.json(result.rows[0]);
-    } catch (err) {
-        console.error('Erro ao editar produto:', err);
-        res.status(500).send('Erro interno ao processar a solicitação');
-    }
-});
-
-app.delete('/produto/:id', async (req, res) => {
-    const { id } = req.params;
-    const queryText = 'DELETE FROM produto WHERE id = $1 RETURNING *';
-    const values = [id];
-
-    try {
-        const result = await pool.query(queryText, values);
-        res.json(result.rows[0]);
-    } catch (err) {
-        console.error('Erro ao deletar produto:', err);
-        res.status(500).send('Erro interno ao processar a solicitação');
-    }
-});
-
-app.post('/funcionario', async (req, res) => {
-    const { nome, datanascimento, cpf, celular, endereco, funcao } = req.body;
-    const queryText = 'INSERT INTO funcionario (nome, datanascimento, cpf, celular, endereco, funcao) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
-    const values = [nome, datanascimento, cpf, celular, endereco, funcao];
-
-    try {
-        const result = await pool.query(queryText, values);
-        res.status(201).json(result.rows[0]);
-    } catch (err) {
-        console.error('Erro ao cadastrar funcionário:', err);
-        res.status(500).send('Erro interno ao processar a solicitação');
-    }
-});
-
-app.get('/funcionario', async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM funcionario');
-        res.json(result.rows);
-    } catch (err) {
-        res.status(400).send('Erro ao consultar funcionários');
-    }
-});
-
-app.put('/funcionario/:id', async (req, res) => {
-    const { id } = req.params;
-    const { nome, datanascimento, cpf, celular, endereco, funcao } = req.body;
-    const queryText = 'UPDATE funcionario SET nome = $1, datanascimento = $2, cpf = $3, celular = $4, endereco = $5, funcao = $6 WHERE id = $7 RETURNING *';
-    const values = [nome, datanascimento, cpf, celular, endereco, funcao, id];
-
-    try {
-        const result = await pool.query(queryText, values);
-        res.json(result.rows[0]);
-    } catch (err) {
-        console.error('Erro ao editar funcionário:', err);
-        res.status(500).send('Erro interno ao processar a solicitação');
-    }
-});
-
-app.delete('/funcionario/:id', async (req, res) => {
-    const { id } = req.params;
-    const queryText = 'DELETE FROM funcionario WHERE id = $1 RETURNING *';
-    const values = [id];
-
-    try {
-        const result = await pool.query(queryText, values);
-        res.json(result.rows[0]);
-    } catch (err) {
-        console.error('Erro ao deletar funcionário:', err);
-        res.status(500).send('Erro interno ao processar a solicitação');
-    }
-});
-
+// Página de dados (relatórios, etc)
 app.get('/dados', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'dados.html'));
+    res.sendFile(path.join(__dirname, 'app', 'indexl'));
 });
 
 app.listen(port, () => {
-    console.log(`Servidor tá on! http://localhost:${port}`);
+    console.log(`Servidor está rodando na URL http://localhost:${port}`);
 });
