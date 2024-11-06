b72ded9811375c2499c2bad46f19463b1f42268e
Diego Confessor <diegondonascimento@gmail.com>
2024-11-06 10:32:51 -0300

    melhorias

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMkr_fkfRJ_5aOR2nnel_-Q87sknlbLbA",
+  authDomain: "auto-park-3cb09.firebaseapp.com",
+  databaseURL: "https://auto-park-3cb09-default-rtdb.firebaseio.com",
+  projectId: "auto-park-3cb09",
+  storageBucket: "auto-park-3cb09.firebasestorage.app",
+  messagingSenderId: "953815140718",
+  appId: "1:953815140718:web:e6fe39be89be28e3c3197f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
